c301f403d6745b68d8e18772ac546aac52e0f2b3
Michael Nicolaou <mikenicolaou1994@gmail.com>
2019-11-16 21:21:33 -0700

    buncha shit

---
+++ ontab/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Landing from './Components/Landing';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Landing></Landing>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
